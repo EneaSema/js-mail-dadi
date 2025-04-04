c41cf2df8fe861c28a0c06e20a4e953fff03e90b
EneaSema <sema.enea1993@gmail.com>
2025-04-04 18:29:48 +0200

    solution-second-exercise-ugual-number

--- js-dadi/javascript/dadi.js
+++ js-dadi/javascript/dadi.js
@@ -17,6 +17,42 @@ console.log(numberPc);
 
 if (numberUser > numberPc) {
   console.log("Hai vinto");
-} else {
+} else if (numberUser < numberPc) {
   console.log("Hai perso");
+} else {
+  console.log("Pareggio");
+}
+const outputText = "Vuoi rigiocare?";
+console.log(outputText);
+
+let responseUser = prompt();
+console.log(responseUser);
+const verificated = responseUser =
+
+if (responseUser === "si") {
+  console.log(responseUser);
+  let numberUser = 0;
+  numberUser = Math.floor(Math.random() * 6 + 1);
+  console.log(numberUser);
+
+  let numberPc = 0;
+  numberPc = Math.floor(Math.random() * 6 + 1);
+  console.log(numberPc);
+
+  if (numberUser > numberPc) {
+    console.log("Hai vinto");
+  } else if (numberUser < numberPc) {
+    console.log("Hai perso");
+  } else {
+    console.log("Pareggio");
+  }
+  const outputText = "Vuoi rigiocare?";
+  console.log(outputText);
+  let responseUser = prompt("si/no?");
+  console.log(responseUser);
+} else if (responseUser === "no") {
+  console.log(responseUser);
+  console.log("Ok, alla prossima");
+} else {
+  console.log("Attendo una tua risposta");
 }
